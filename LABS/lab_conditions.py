db5bf7d608f2f08a687b50325c8be7c0e210b553
Vieiradu <duullivee@gmail.com>
2026-05-12 10:39:26 -0300

    lab - Calculadora de ano Bissexto

--- LABS/lab_conditions.py
+++ LABS/lab_conditions.py
@@ -70,17 +70,53 @@ Nota: esse país feliz nunca devolveu dinheiro para seus cidadãos. Se o imposto
 Observe o código no editor: ele só lê um valor de entrada e gera um resultado, então você precisa concluí-lo com alguns cálculos inteligentes.
 '''
 
-income = float(input("Entre com os rendimentos "))
+# income = float(input("Entre com os rendimentos "))
 
-if income < 85528:
-    tax = income * 0.18 - 556.02
-else:
-    tax = (income - 85528) * 0.32 + 14839.02
+# if income < 85528:
+#     tax = income * 0.18 - 556.02
+# else:
+#     tax = (income - 85528) * 0.32 + 14839.02
  
-tax = round(tax, 0)
+# tax = round(tax, 0)
 
-tax = max(0, tax)
+# tax = max(0, tax)
 
-print(
- "A taxa é:", tax, "thalers"
- )
+# print(
+#  "A taxa é:", tax, "thalers"
+#  )
+
+'''
+Como você certamente sabe, devido a algumas razões astronômicas, os anos podem ser bissextos ou comuns. Os primeiros têm 366 dias, enquanto os segundos têm 365 dias.
+
+Desde a introdução do calendário gregoriano (em 1582), a regra a seguir é usada para determinar o tipo de ano:
+
+se o número do ano não é divisível por quatro, é um ano comum;
+caso contrário, se o número do ano não for divisível por 100, será um ano bissexto;
+caso contrário, se o número do ano não for divisível por 400, é um ano comum ;
+caso contrário, é um ano bissexto .
+Observe o código no editor: ele só lê o número de um ano e precisa ser concluído com as instruções de implementação do teste que acabamos de descrever.
+
+O código deve gerar uma das duas mensagens possíveis, que são ano bissexto ou ano comum, dependendo do valor inserido.
+
+Seria bom verificar se o ano inserido cai na era gregoriana e emitir um aviso caso contrário: não está dentro do período do calendário gregoriano. Dica: use os operadores != E %.
+
+Teste seu código usando os dados que fornecemos.
+'''
+
+year = int(input("Digite um ano: "))
+
+if year < 1582:
+  print("Não dentro do período do calendário gregoriano")
+else:
+    if year % 4 != 0:
+        print(f"O ano {year} é comum.")
+    elif year % 100 != 0:
+        print(f"O ano {year} é bissexto.")
+    elif year % 400 != 0:
+        print(f"O ano {year} é comum.")
+    else:
+        print(f"O ano {year} é bissexto.")
+
+
+  
+ 
