d4cb1df17c7a8d18827704407fdb1a82745d63b7
Vieiradu <duullivee@gmail.com>
2026-05-12 22:56:49 -0300

    Lab Loop For - 1 mississippi

---
+++ LABS/lab_loop_for.py
@@ -0,0 +1,11 @@
+import time
+
+# Escreva um loop for que conte até cinco
+   # Corpo do loop - exiba o número de iteração do loop e a palavra "Mississippi".
+   # Corpo do loop - use: time.sleep(1)
+for i in range(1, 6):
+    time.sleep(1)
+    print(f"{i} Mississippi")
+print("Pronto ou não aqui vou eu!")
+
+# Escreva uma função print com a mensagem final.
